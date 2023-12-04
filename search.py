75ab5cde739236562f7066ba45549b768f6b77a5
jherico22 <150562578+jherico22@users.noreply.github.com>
2023-12-04 02:40:30 +0000

    Create search.py

---
+++ search.py
@@ -0,0 +1,9 @@
+
+import webbrowser
+
+def open_twitter_profile():
+    twitter_profile_url = "https://twitter.com/JhericoMcl96305"
+    webbrowser.open_new_tab(twitter_profile_url)
+
+if __name__ == "__main__":
+    open_twitter_profile()
